f82b52acc55f0c132b11046337c012c7ee393a44
Kyle Woodard <kyle.p.woodard@gmail.com>
2015-07-22 19:24:52 -0400

    Creating a simple vb program

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Public counter As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        counter = counter + 1
+        If counter <= 3 Then
+            TextBox1.Text = "Hello Label!"
+        ElseIf counter <= 6 Then
+            TextBox1.Text = "Okay thats enough label."
+        ElseIf counter = 7 Then
+            TextBox1.Text = "Okay thats enough label.."
+        ElseIf counter = 8 Then
+            TextBox1.Text = "Okay thats enough label..."
+        Else
+            TextBox1.Text = "Take that!"
+            Label1.Text = "Ughhhh. I got stabbed"
+        End If
+    End Sub
+End Class
